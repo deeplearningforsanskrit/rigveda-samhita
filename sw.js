d65d8944cdc4d3af98865d9aa459f19977cdfd12
abhijitavadhootdeo2001@gmail.com <f20190041@goa.bits-pilani.ac.in>
2026-06-06 16:57:49 +0530

    lets make it available offline!

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'rigveda-v1';
+
+// All paths are relative to the location of sw.js
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  "./style.css",
+  "./rigveda.json",
+  "./app.js"
+  // Add your specific css, js, or json data paths below:
+  // './css/style.css',
+  // './js/app.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      // Serve from cache if available, otherwise fetch from GitHub network
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
